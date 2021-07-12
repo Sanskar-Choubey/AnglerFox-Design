956c5609e7a4a3a8e1a1094f6587233240548ba2
Sanskar Choubey <choubeysanskar03@gmail.com>
2021-07-12 16:01:21 +0530

    fix: hamburger icon

--- navbar.js
+++ navbar.js
@@ -2,3 +2,14 @@ window.addEventListener('scroll',function(){
     let nav = document.querySelector('nav');
     nav.classList.toggle("stikcy" , window.scrollY > 0);
 })
+
+const button = document.querySelector('#mobile-view');
+const navLinks = document.querySelector('.nav-links ul');
+
+button.addEventListener('click',function(){
+    navLinks.classList.toggle('active')
+})
+
+
+console.log(button);
+console.log(navLinks);
